7da3f73bd100d06de4763c1cf856cb473e1d04a4
Richard Nguyen <richard.ng0616@gmail.com>
2022-12-13 11:48:39 -0800

    Finish wzip.c to match all requirements

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -2,14 +2,54 @@
  * @file wzip.c
  * @author RIchard Nguyen (richard@richardhnguyen.com)
  * @brief Simple compression program.
- * @version 0.1
+ * @version 0.2
  * @date 2022-12-12
  *
  * @copyright Copyright (c) 2022
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>	// FILE, stdout, fprintf, fwrite, fgetc, EOF, fclose
+#include <stdlib.h> // EXIT_*, exit
+
+struct rle_t
+{
+	int l;
+	char c;
+};
+
+void
+writerle(struct rle_t rleobj)
+{
+	fwrite((int *)(&(rleobj.l)), sizeof(int), 1, stdout);
+	fwrite((char *)(&(rleobj.c)), sizeof(char), 1, stdout);
+}
+
+struct rle_t
+process(FILE *stream, struct rle_t prev)
+{
+	int curr;
+	struct rle_t rle;
+
+	while ((curr = fgetc(stream)) != EOF)
+	{
+		if (prev.c != '\0' && curr != prev.c)
+		{
+			rle.c  = prev.c;
+			rle.l  = prev.l;
+			prev.l = 0;
+
+			writerle(rle);
+		}
+
+		prev.l++;
+		prev.c = curr;
+	}
+
+	rle.c = prev.c;
+	rle.l = prev.l;
+
+	return rle;
+}
 
 int
 main(int argc, const char *argv[])
@@ -19,6 +59,10 @@ main(int argc, const char *argv[])
 		fprintf(stdout, "wzip: file1 [file2 ...]\n");
 		exit(EXIT_FAILURE);
 	}
+
+	struct rle_t prev;
+	prev.c = '\0';
+	prev.l = 0;
 
 	for (int i = 1; i < argc; ++i)
 	{
@@ -31,13 +75,14 @@ main(int argc, const char *argv[])
 			exit(EXIT_FAILURE);
 		}
 
-		int len = 38;
-		char c	= 'a';
-		fwrite((int *)(&len), sizeof(int), 1, stdout);
-		fwrite((char *)(&c), sizeof(char), 1, stdout);
+		struct rle_t rle = process(fp, prev);
+		prev.c			 = rle.c;
+		prev.l			 = rle.l;
 
 		fclose(fp);
 	}
+
+	writerle(prev);
 
 	return 0;
 }
