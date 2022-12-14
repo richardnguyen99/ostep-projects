86a5c4dd630ae1ac18249fa0529786541fcfc924
Richard Nguyen <richard.ng0616@gmail.com>
2022-12-13 17:30:36 -0800

    Finish reverse.c

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -2,7 +2,7 @@
  * @file reverse.c
  * @author Richard Nguyen (richard@richardhnguyen.com)
  * @brief A simple stream-reversing program.
- * @version 0.3
+ * @version 0.4
  * @date 2022-12-13
  *
  * @copyright Copyright (c) 2022
@@ -11,6 +11,10 @@
 #include <stdio.h>	// FILE, stdout, fprintf, fclose
 #include <stdlib.h> // EXIT_*, exit
 #include <string.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 char **
 linesalloc(char **oldptr, size_t size)
@@ -73,6 +77,18 @@ inplace_reverse(char **lines, size_t n)
 }
 
 int
+check_inode(const char *f1, const char *f2)
+{
+	struct stat buf1 = {0};
+	lstat(f1, &buf1);
+
+	struct stat buf2 = {0};
+	lstat(f2, &buf2);
+
+	return buf1.st_ino == buf2.st_ino;
+}
+
+int
 main(int argc, const char *argv[])
 {
 	if (argc > 3)
@@ -85,24 +101,57 @@ main(int argc, const char *argv[])
 	size_t cnt	 = 0;
 	char **lines = NULL;
 
-	if (argc == 2)
-	{
-		FILE *fp = fopen(argv[1], "r");
+	FILE *ifstream = stdin;
+	FILE *ofstream = stdout;
 
-		if (fp == NULL)
+	if (argv[1])
+	{
+		ifstream = fopen(argv[1], "r");
+
+		if (ifstream == NULL)
+		{
+			fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (ifstream != stdin && argv[2])
+	{
+		if (strcmp(argv[1], argv[2]) == 0)
+		{
+			fprintf(stderr, "reverse: input and output file must differ\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if (check_inode(argv[1], argv[2]))
+		{
+			fprintf(stderr, "reverse: input and output file must differ\n");
+			exit(EXIT_FAILURE);
+		}
+
+		ofstream = fopen(argv[2], "w+");
+
+		if (ofstream == NULL)
 		{
 			fprintf(stderr, "reverse: cannot open files\n");
 			exit(EXIT_FAILURE);
 		}
-
-		lines = getlines(fp, &cnt, &cap);
-		fclose(fp);
 	}
 
+	lines = getlines(ifstream, &cnt, &cap);
 	inplace_reverse(lines, cnt);
 
 	for (size_t i = 0; i < cnt; ++i)
+	{
+		fprintf(ofstream, "%s", lines[i]);
 		free(lines[i]);
+	}
+
+	if (ifstream != stdin)
+		fclose(ifstream);
+	if (ofstream != stdout)
+		fclose(ofstream);
+
 	free(lines);
 
 	return 0;
